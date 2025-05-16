f3e274deba44c9d6f9eebf8be69566b1c8a3e6fb
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-05-16 22:13:33 +0300

    Added support for multiple MCP expansion chips

--- arduino/hotas/hotas.ino
+++ arduino/hotas/hotas.ino
@@ -6,15 +6,37 @@
   - pushbutton attached from pin to GND
   - 22 kilohm resistor attached from pin to GND
   - potentiometer(s) have
-    - 1 connected to +5V
-    - 2 analog input pin
-    - 3 connected to GND
+    - pin 1 connected to +5V
+    - pin 2 analog input pin
+    - pin 3 connected to GND
 
-  - MCP23017
-    - SDL (pin 12) connected to A5 on Arduino
-    - SDA (pin 13) connected to A4 on Arduino
+  - MCP23017 (1)
+    - SDL (pin 12) to A5 on Arduino
+    - SDA (pin 13) to A4 on Arduino
     - VDD (pin 9) & RESET (pin 18) connected to +5V 
-    - VSS (pin 10) & A0-A2 (pins 15-17)
+    - VSS (pin 10) to GND
+    - A0 (pins 15) to GND
+    - A1 (pins 16) to GND
+    - A2 (pins 17) to GND
+
+  - MCP23017 (2)
+    -  ''
+    - A0 (pins 15) to +5V
+    - A1 (pins 16) to GND
+    - A2 (pins 17) to GND
+
+Pins A0-A2 are used to express the I2C address of the chip in binary
+
+      | A2  |  A1 |  A0 | Address
+  000 | GND | GND | GND | 0x20
+  001 | GND | GND |  5V | 0x21
+  010 | GND |  5V | GND | 0x22
+  011 | GND |  5V |  5V | 0x23
+  100 |  5V | GND | GND | 0x24
+  ... | ... | ... | ... | ...
+  111 |  5V |  5V |  5V | 0x27
+
+Up to eight (8) chips can be connected on the same I2C pins
 
   Created 16.06.2024
   Updated 13.05.2025
@@ -23,13 +45,6 @@
   http://www.arduino.cc/en/Tutorial/Debounce
   https://docs.arduino.cc/built-in-examples/analog/AnalogInput/
 */
-
-// ADDS MCP 23017 SUPPORT //
-// Coumment out to remove
-
-#include <Adafruit_MCP23X17.h>
-
-Adafruit_MCP23X17 mcp;
 
 //// START OF CONFIGURATION ////
 
@@ -41,13 +56,15 @@ const bool i2c = true;              // enable I2C if you have an MCP23X17 connec
 // Remember to update the input arrays with correct input pin IDs.
 const int btns = 11;                // Number of buttons
 const int alogs = 2;                // Number of analog inputs
-const int mcps = 16;
+const int mcps = 16;                // Number of input pins on MCP chip
+const int chips = 2;                // Number of chips connected
 
 // I/O pin ID numbers:
 const int btnPin[btns] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };     // Array of button pins: Define the digital input pins here
 const int analogPin[alogs] = { A2, A3 };    // Array of analog pins: Define the analog input pins here
-const int ledPin = 13;              // the number of the LED pin (13 is the internal LED)
+const int ledPin = 13;                      // the number of the LED pin (13 is the internal LED)
 const int mcpBtnPin[mcps] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+const uint8_t chipAddr[chips] = { 0x20, 0x21 };       // the addresses of MCP chips
 
 // Debounse
 unsigned long debounceDelay = 10;   // the debounce time; increase if the output flickers
@@ -57,6 +74,12 @@ const int analogDeadzone = 3;
 
 //// END OF CONFIGURATION ////
 
+// ADDS MCP 23017 SUPPORT //
+// Coumment out to remove
+
+#include <Adafruit_MCP23X17.h>
+
+Adafruit_MCP23X17* mcp[chips];
 
 // LED state information
 int ledState = HIGH;             // the current state of the LED pin
@@ -68,9 +91,9 @@ int btnPrevious[btns] = { };      // the previous reading from the input pins
 int btnTempValue[btns] = { };     // temp buffer for values
 
 // MCP Button state information
-int mcpBtnValue[mcps] = { };         // the current reading from the input pins
-int mcpBtnPrevious[mcps] = { };      // the previous reading from the input pins
-int mcpBtnTempValue[mcps] = { };     // temp buffer for values
+int mcpBtnValue[chips][mcps] = { };         // the current reading from the input pins
+int mcpBtnPrevious[chips][mcps] = { };      // the previous reading from the input pins
+int mcpBtnTempValue[chips][mcps] = { };     // temp buffer for values
 
 // Analog state information
 int analogValue[alogs] = { };     // the current reading from the analog pins
@@ -78,9 +101,18 @@ int analogPrevious[alogs] = { };  // the previous reading from the input pins
 
 // milliseconds, will quickly become a bigger number than can be stored in an int
 unsigned long lastDebounceTime[btns] = { };  // the last time the output pin was toggled
-unsigned long mcpLastDebounceTime[mcps] = { };  // the last time the output pin was toggled
+unsigned long mcpLastDebounceTime[chips][mcps] = { };  // the last time the output pin was toggled
 unsigned long ledTime;  // the last time the output pin was toggled
 
+
+// Initializes I2C to chip
+void setupMcpChip(int chip, uint8_t addr) {
+  mcp[chip] = new Adafruit_MCP23X17();
+  if (!mcp[chip]->begin_I2C(addr)) {
+    Serial.println("Error.");
+    while (1);
+  }
+}
 
 // Sets up all buttons pins as inputs
 void setupButtons() {
@@ -89,10 +121,10 @@ void setupButtons() {
   }
 }
 
-
-void setupMcpButtons() {
+// Sets up all MCP buttons pins as inputs
+void setupMcpButtons(Adafruit_MCP23X17* chip) {
   for ( int i = 0; i < mcps ; i++ ) {
-    mcp.pinMode(mcpBtnPin[i], INPUT_PULLUP);
+    chip->pinMode(mcpBtnPin[i], INPUT_PULLUP);
   }
 }
 
@@ -178,44 +210,58 @@ void readBtn() {
 // Reads digital inputs from MCP2017 and updates the arrays
 // uses debounce
 void readMCP() {
-  for ( int i = 0; i < mcps; i++ ) {
-    mcpBtnTempValue[i] = mcp.digitalRead(mcpBtnPin[i]);
+  for ( int chip = 0; chip < chips; chip++ ) {
+    for ( int i = 0; i < mcps; i++ ) {
+      mcpBtnTempValue[chip][i] = mcp[chip]->digitalRead(mcpBtnPin[i]);
 
-    // If a change is seen on input
-    if (mcpBtnTempValue[i] != mcpBtnPrevious[i]) {
-      // reset the debouncing timer
-      lastDebounceTime[i] = millis();
-    }
-
-    // If the debounse timeout has passed
-    if ((millis() - mcpLastDebounceTime[i]) > debounceDelay) {
-
-      if (mcpBtnTempValue[i] != mcpBtnValue[i]) {
-        // Set button value
-        mcpBtnValue[i] = mcpBtnTempValue[i];
-
-        // Send button event on serial
-        sendValue("MCP", i, !mcpBtnValue[i]);
-        flashLED(HIGH);
+      // If a change is seen on input
+      if (mcpBtnTempValue[chip][i] != mcpBtnPrevious[chip][i]) {
+        // reset the debouncing timer
+        mcpLastDebounceTime[chip][i] = millis();
       }
+
+      // If the debounse timeout has passed
+      if ((millis() - mcpLastDebounceTime[chip][i]) > debounceDelay) {
+
+        if (mcpBtnTempValue[chip][i] != mcpBtnValue[chip][i]) {
+          // Set button value
+          mcpBtnValue[chip][i] = mcpBtnTempValue[chip][i];
+
+          // Send button event on serial
+          sendValue("MCP"+String(chipAddr[chip])+".", i, !mcpBtnValue[chip][i]);
+          flashLED(HIGH);
+        }
+      }
+      mcpBtnPrevious[chip][i] = mcpBtnTempValue[chip][i];
     }
-    mcpBtnPrevious[i] = mcpBtnTempValue[i];
   }
 }
 
 
 void setup() {
-  setupButtons();
-  setupLEDs();
   Serial.begin(9600);   // initialize serial comm at 9600 bits per second
+  Serial.println("# Starting...");
+  Serial.println("# Setting up buttons...");
+  setupButtons();
+  Serial.println("# Setting up LEDs...");
+  setupLEDs();
 
-  if (i2c) {
-    if (!mcp.begin_I2C()) {
-      Serial.println("Error.");
-      while (1);
-    }
-    setupMcpButtons();
+  delay(100);
+
+  if (!i2c) {
+    Serial.println("# No i2c");
+    Serial.println("# Ready");
+    return;
   }
+
+  for (int chip = 0; chip < chips; chip++ ) {
+    Serial.println("# Setting up MCP...: " + String(chip));
+    setupMcpChip(chip, chipAddr[chip]);
+    Serial.println("# Setting MCP addr: "+String(chipAddr[chip]));
+    Serial.println("# Setting MCP buttons...");
+    setupMcpButtons(mcp[chip]);
+  }
+  Serial.println("# Ready");
 }
 
 void loop() {
